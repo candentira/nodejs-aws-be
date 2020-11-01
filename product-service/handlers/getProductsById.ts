b7083e597ca489add9fdd3c277f9b47c34718aba
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-11-01 17:43:41 +0300

    feat(hw3): add error handling for product-service

--- product-service/handlers/getProductsById.ts
+++ product-service/handlers/getProductsById.ts
@@ -1,19 +1,48 @@
 import 'source-map-support/register';
+import productList from './productList.json'
+import { PRODUCT_ERRORS } from './productConsts'
 
-let productList
+// Lazy loading only to show that async await would be used in this lambda
+// const productGetter = () => {
+//   let products
+//   return async () => {
+//     if (!products) {
+//       //products = await import('./productList.json').then(list => list.default )
+//       throw Error('WOW Error')
+//     }
+//     return products
+//   }
+// }
+// const getProducts = productGetter()
 
 export default async (event, _context) => {
-  const { id } = event.pathParameters
-  if (!productList) {
-    productList = await import('./productList.json').then(list => list.default )
+  const responseHeaders = { headers: { 'Access-Control-Allow-Origin': '*' } }
+  try {
+    // const productList = await getProducts()
+    const { id } = event.pathParameters
+    if (!id) {    
+      return { ...responseHeaders,
+        statusCode: 400,
+        body: JSON.stringify(PRODUCT_ERRORS.ID_NOT_PROVIDED),
+      };
+    }
+    const product = productList.find(product => product.id === id)
+    if (!product) {
+      return { ...responseHeaders,
+        statusCode: 404,
+        body: JSON.stringify(PRODUCT_ERRORS.NOT_FOUND),
+      };
+    }
+    return { ...responseHeaders,
+      statusCode: 200,
+      body: JSON.stringify(product),
+    }
+  } catch(err) {
+    console.error(err)    
+    return { ...responseHeaders,
+      statusCode: 500,
+      body: JSON.stringify(PRODUCT_ERRORS.SERVER_ISSUE),
+    };
   }
-  const products = productList.filter(product => product.id === id)
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: JSON.stringify(products),
-  };
 }
 
---
+++ product-service/handlers/productConsts.ts
@@ -0,0 +1,5 @@
+export const PRODUCT_ERRORS = {
+    NOT_FOUND: 'Product not found',
+    ID_NOT_PROVIDED: 'Invalid ID provided',
+    SERVER_ISSUE: 'Something went wrong',
+}
