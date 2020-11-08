43515c968518422a29ad8fa076f8d2c2c250883b
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-11-08 21:09:56 +0300

    feat(hw4): createProducts lambda returns 400 status code if wrong data provided

--- product-service/handlers/createProducts.ts
+++ product-service/handlers/createProducts.ts
@@ -2,15 +2,30 @@ import 'source-map-support/register'
 import { createProducts } from './services/productsService'
 import { PRODUCT_ERRORS } from './productConsts'
 
+const isValidRequest = ({ title, description, price, count }) => {
+    return title && description && price && count &&
+        typeof price === 'number' && typeof count === 'number' &&
+        typeof title === 'string' && typeof description === 'string'
+}
+
 export default async event => {
   console.log("createProducts Lambda started execution");
   console.info("ENVIRONMENT VARIABLES\n" + JSON.stringify(process.env, null, 2));
   console.log("EVENT\n" + JSON.stringify(event, null, 2));
   
   const responseHeaders = { headers: { 'Access-Control-Allow-Origin': '*' } };
-  try{
+  try {
     const { title, description, price, count } = JSON.parse(event.body);
-    console.log(`CREATE PRODUCT: ${title}, ${description}, ${price}, ${count}`);
+    if(!event.body || !isValidRequest({ title, description, price, count })) {
+        console.info("createProducts Lambda didn't create product: wrong data provided");
+
+        return {
+          ...responseHeaders,
+          statusCode: 400,
+          body: JSON.stringify(PRODUCT_ERRORS.INVALID_PRODUCT_DATA)
+        }
+    }
+
     await createProducts({ title, description, price, count });
     
     console.info("createProducts Lambda finished execution successfully");
--- product-service/handlers/productConsts.ts
+++ product-service/handlers/productConsts.ts
@@ -2,4 +2,5 @@ export const PRODUCT_ERRORS = {
     NOT_FOUND: 'Product not found',
     ID_NOT_PROVIDED: 'Invalid ID provided',
     SERVER_ISSUE: 'Something went wrong',
+    INVALID_PRODUCT_DATA: 'Provided product data is invalid'
 }
