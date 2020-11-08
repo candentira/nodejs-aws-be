4ff983e735d0fe0f6afc09d48f598a4eab398045
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-11-08 20:14:19 +0300

    feat(hw4): return 500 status code on any error

--- product-service/handlers/createProducts.ts
+++ product-service/handlers/createProducts.ts
@@ -1,14 +1,22 @@
 import 'source-map-support/register'
 import { createProducts } from './services/productsService'
+import { PRODUCT_ERRORS } from './productConsts'
 
-export default async event => {
-  const { title, description, price, count } = JSON.parse(event.body)
-  console.log(`CREATE PRODUCT: ${title}, ${description}, ${price}, ${count}`)
-  await createProducts({ title, description, price, count })
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
+export default async event => {    
+  const responseHeaders = { headers: { 'Access-Control-Allow-Origin': '*' } }
+  try{
+    const { title, description, price, count } = JSON.parse(event.body)
+    console.log(`CREATE PRODUCT: ${title}, ${description}, ${price}, ${count}`)
+    await createProducts({ title, description, price, count })
+    return {
+      ...responseHeaders,
+      statusCode: 200,
+    }
+  } catch(err) {
+    return {
+      ...responseHeaders,
+      statusCode: 500,
+      body: JSON.stringify(PRODUCT_ERRORS.SERVER_ISSUE)
+    }
   }
 }
--- product-service/handlers/getProductsList.ts
+++ product-service/handlers/getProductsList.ts
@@ -1,13 +1,21 @@
 import 'source-map-support/register'
 import { getProductsList } from './services/productsService'
+import { PRODUCT_ERRORS } from './productConsts'
 
 export default async event => {
-  let productList = await getProductsList()
-  return {
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-    statusCode: 200,
-    body: JSON.stringify(productList),
+  const responseHeaders = { headers: { 'Access-Control-Allow-Origin': '*' } }
+  try{
+    let productList = await getProductsList()
+    return {
+      ...responseHeaders,
+      statusCode: 200,
+      body: JSON.stringify(productList),
+    }
+  } catch(err) {
+    return {
+      ...responseHeaders,
+      statusCode: 500,
+      body: JSON.stringify(PRODUCT_ERRORS.SERVER_ISSUE)
+    }
   }
 }
