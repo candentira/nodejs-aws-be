5c0acc54b73348a043831e8d3863a3da296f60a8
ViktoryiaKlimenka <Viktoryia_Klimenka@epam.com>
2020-11-29 20:14:53 +0300

    feat(hw7): add authorization-service, basicAuthorizer lambda function, import-service uses basicAuthorizer function

---
+++ authorization-service/handlers/basicAuthorizer.ts
@@ -0,0 +1,42 @@
+export default async (event, ctx, cb) => {
+    console.log("basicAuthorizer Lambda started execution");
+    console.log("EVENT\n" + JSON.stringify(event, null, 2));
+
+    if(event.type != 'TOKEN') {
+        cb('Unauthorized')
+    }
+
+    try {
+        const authorizationToken = event.authorizationToken
+        const encodedCreds = authorizationToken.split(' ')[1]
+        const buff = Buffer.from(encodedCreds, 'base64')
+        const plainCreds = buff.toString('utf-8').split(':')
+        const [ username, password ] = plainCreds
+
+        console.log(`username: ${username}, password: ${password}`)
+
+        const storedUserPassword = process.env[username]
+        const effect = !storedUserPassword || storedUserPassword != password ? 'Deny' : 'Allow'
+
+        const policy = generatePolicy(encodedCreds, event.methodArn, effect)
+        cb(null, policy)
+    } catch(e) {
+        cb(`Unauthorized: ${e.message}`)
+    }
+}
+
+const generatePolicy = (principalId, resource, effect = 'Allow') => {
+    return {
+        principalId: principalId,
+        policyDocument: {
+            Version: '2012-10-17',
+            Statement: [
+                {
+                    Action: 'execute-api:Invoke',
+                    Effect: effect,
+                    Resource: resource
+                }
+            ]
+        }
+    }
+}
